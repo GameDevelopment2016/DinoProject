fbaf4fe3563e2fdba7193c212d0f1995649cb32d
Ivan Wesley Chua <ivanwesleychua@yahoo.com>
2016-03-04 23:34:05 +0800

    test

--- Assets/SCRIPTS/PterodactylController.cs
+++ Assets/SCRIPTS/PterodactylController.cs
@@ -2,13 +2,11 @@
 using System.Collections;
 
 public class PterodactylController : MonoBehaviour {
-
-	// Use this for initialization
+	
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 	
 	}
