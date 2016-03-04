baaf1b0976bfc81a32a6a43483a599d225989212
ivanwchua <ivanwchua@gmail.com>
2016-03-04 23:34:28 +0800

    teest

---
+++ Assets/SCRIPTS/TouchScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update () {
+		Touch myTouch = Input.GetTouch (0);
+	}
+}
