4e925979d639e4e92737fffcd986cb9db9ba74d1
ivanwchua <ivanwchua@gmail.com>
2016-03-06 16:27:57 +0800

    dinos

---
+++ Assets/SCRIPTS/Explode.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour {
+
+	void Start() {
+
+	}
+		
+	void Update() {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D target) {
+		if(target.gameObject.tag == "Deadly") {
+			OnExplode();
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D target) {
+		if(target.gameObject.tag == "Deadly") {
+			OnExplode();
+		}
+	}
+
+	public void OnExplode(){
+		Destroy(gameObject);
+	}
+}
